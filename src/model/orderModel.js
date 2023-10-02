2ceeb8f855fcc286fc416371aefa200ab01a069a
Patrick Correia Landra <patrick.landra@telefonica.com>
2023-10-02 11:48:07 -0300

    atualizando acessos

---
+++ src/model/orderModel.js
@@ -0,0 +1,59 @@
+//cria o modelo do banco de dados
+import {Sequelize} from "sequelize";
+import db from "../config/db.js";
+
+const Order = db.define("order", {
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+},
+    nome: {
+        type:Sequelize.STRING(200),
+        allowNull: false,
+    },
+    cpf: {
+        type:Sequelize.INTEGER(15).UNSIGNED,
+        allowNull: false,
+    },
+    telefone:{
+        type:Sequelize.STRING(20),
+        allowNull: false,
+    },
+    Plano:{
+        type:Sequelize.STRING(30),
+        allowNull: false,
+    },
+    horario:{
+        type:Sequelize.DATE,
+        allowNull: false,
+    }
+});
+
+
+const painel_acessos = db.define( "painel_acessos", {
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+},
+    nome: {
+        type:Sequelize.STRING(200),
+        allowNull: false,
+    },
+    senha: {
+        type:Sequelize.STRING(20).UNSIGNED,
+        allowNull: false,
+
+    }
+});
+
+
+
+//criar uma função que converter string para date
+// quando receber uma requisição de criação de uma order
+
+//consumir api FRONT, acessar api, retornar os dados e salvar
+//usar o axxos no front para fazer requisicoes para api e obter e salvar as respostas
+// usar em vendas
+export default (Order,painel_acessos);
